02b72b6dd8b171eb744634300b81ee28daa6d990
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-09 14:31:37 -1000

    28 passing. complete

--- calculator.js
+++ calculator.js
@@ -6,6 +6,22 @@
  * @return {object} `calculator` object that can be used
  */
 
+var calculatorModule = (function(){
+  
+  var memory = 0;
+  var total = 0;
+
+  var calculator = {
+    load: load,
+    getTotal: getTotal,
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory: recallMemory,
+    saveMemory: saveMemory,
+    clearMemory: clearMemory
+  };
 
   /**
    * sets the `total` to the number passed in
@@ -13,53 +29,98 @@
    * @return { Number }    current total
    */
 
+   function load(x){
+    validate(x);
+    total = x;
+    return total;
+   }
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
 
+   function getTotal(x){
+    return total;
+   }
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
 
+   function add(x){
+    validate(x);
+    total += x;
+    return total;
+   }
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
 
+   function subtract(x){
+    validate(x);
+    total -= x;
+    return total;
+   }
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
 
+   function multiply(x){
+    validate(x);
+    total *= x;
+    return total;
+   }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+   function divide(x){
+    validate(x);
+    total /= x;
+    return total;
+   }
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+   function recallMemory(){
+    return memory;
+   }
 
   /**
    * Stores the value of `total` to `memory`
    */
 
+   function saveMemory(){
+    memory = total;
+   }
 
   /**
    * Clear the value stored at `memory`
    */
 
+   function clearMemory(){
+    memory = 0;
+   }
+
   /**
    * Validation
    */
 
+   function validate(x){
+    if (typeof x !== 'number'){
+      throw new Error();
+    }
+   }
+   return calculator;
+});
